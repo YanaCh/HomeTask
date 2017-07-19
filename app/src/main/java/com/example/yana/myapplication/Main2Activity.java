ed61a07a74fb50a295236ccf3f39a92ded46b80f
YanaCh <cherepinina98@gmail.com>
2017-07-19 14:15:09 +0300

    Working Version

--- app/src/main/java/com/example/yana/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/yana/myapplication/Main2Activity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,7 +16,7 @@ public class Main2Activity extends AppCompatActivity {
     public final static String EXTRA_SEND =
             "com.example.yana.myapplication.send";
     public static final String EXTRA_ANSWER = "com.example.yana.myapplication.answer";
-    public static  String EXTRA_ANSWER_REJECT = "com.example.yana.myapplication.answer_reject";
+
 
     private String mTextSend;
     private Button mConfirmButton;
@@ -28,51 +29,54 @@ public class Main2Activity extends AppCompatActivity {
             return i;
     }
 
-    private void setAnswer(String answer) {
-        Intent data = new Intent(Main2Activity.this,MainActivity.class);
-        startActivity(data);
+    private void setAnswerReject(String answer) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER, answer);
+        setResult(RESULT_CANCELED, data);
+        finish();
+            }
+
+    private void setAnswerConfirm(String answer) {
+        Intent data = new Intent();
         data.putExtra(EXTRA_ANSWER, answer);
         setResult(RESULT_OK, data);
-    }
-    public static String wasAnswer(Intent result){
-        return result.getStringExtra(EXTRA_ANSWER);
-    }
-
-    private void setAnswerReject(String answer) {
-        Intent data = new Intent(Main2Activity.this,MainActivity.class);
-        data.putExtra(EXTRA_ANSWER_REJECT, answer);
-        setResult(RESULT_CANCELED, data);
-        startActivity(data);
-    }
-    public static String wasAnswerReject(Intent result){
-        return result.getStringExtra(EXTRA_ANSWER_REJECT);
+        finish();
+            }
+    public static String wasAnswer(Intent result){return result.getStringExtra(EXTRA_ANSWER);
     }
 
+    @Override
+    public void onBackPressed() {
+        Intent i = new Intent(Main2Activity.this, MainActivity.class);
+        startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        mTextSend = getIntent().getStringExtra(EXTRA_SEND);
 
         mConfirmButton = (Button)findViewById(R.id.confirm_button);
         mRejectButton = (Button)findViewById(R.id.reject_button);
         mEditText = (EditText)findViewById(R.id.edit_text);
 
+        mTextSend = getIntent().getStringExtra(EXTRA_SEND);
         mEditText.setText(mTextSend);
 
         mConfirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setAnswer("");
+                setAnswerConfirm("");
+
             }
         });
 
         mRejectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setAnswerReject(mTextSend);
+                setAnswerReject( mTextSend);
+
             }
         });
 
--- app/src/main/java/com/example/yana/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yana/myapplication/MainActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mClearButton;
@@ -16,48 +18,51 @@ public class MainActivity extends AppCompatActivity {
 
     private final static int REQUEST_SEND = 0;
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_SEND) {
-            if(resultCode == RESULT_OK) {
-                if (data != null) {
-                    mAnswer = Main2Activity.wasAnswer(data);
-                    mInputEditText.setText(mAnswer);
-                }
-            }
-            if (resultCode == Activity.RESULT_CANCELED) {
-                mAnswer = Main2Activity.wasAnswerReject(data);
-                mInputEditText.setText(mAnswer);
-            }
-        }
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mInputEditText = (EditText)findViewById(R.id.input_edit_text);
+        mInputEditText = (EditText) findViewById(R.id.input_edit_text);
 
         mClearButton = (Button) findViewById(R.id.clear_button);
         mClearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               mInputEditText.setText("");
+                mInputEditText.setText("");
             }
         });
 
-        mSendButton = (Button)findViewById(R.id.send_button);
+        mSendButton = (Button) findViewById(R.id.send_button);
         mSendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String text = mInputEditText.getText().toString();
-                    Intent i = Main2Activity.newIntent(MainActivity.this, text);
-                    startActivityForResult(i, REQUEST_SEND);
+                    if(!mInputEditText.getText().toString().equals("")) {
+                        String text = mInputEditText.getText().toString();
+                        Intent i = Main2Activity.newIntent(MainActivity.this, text);
+                        startActivityForResult(i, REQUEST_SEND);
+                    }
 
             }
         });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_SEND && data != null) {
+            if(resultCode == Activity.RESULT_OK){
+                Toast.makeText(this, R.string.info_confirm_toast, Toast.LENGTH_SHORT)
+                        .show();
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(this, R.string.info_reject_toast, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+        mAnswer = Main2Activity.wasAnswer(data);
+        mInputEditText.setText(mAnswer);
+         }
 }
